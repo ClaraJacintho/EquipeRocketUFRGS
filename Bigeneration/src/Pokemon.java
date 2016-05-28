337975b64abe6e31e4a9bc20d2d74c9315432206
SpaceCenturion <mcmachry@gmail.com>
2016-05-28 18:12:09 -0300

    Update Pokemon.java
    Intantiateted moves

--- Bigeneration/src/Pokemon.java
+++ Bigeneration/src/Pokemon.java
@@ -6,7 +6,7 @@ public class Pokemon {
 	int evolution;	//Code of this Pokemons evolution
 	int prevolution;	//The Pokemon that evolves into this 
 	
-	ArrayList<Integer> moves;  //List of all possible moves this pokemon can have (max Mew-114) - Stores move's code
+	ArrayList<Integer> moves = new ArrayList<>();   //List of all possible moves this pokemon can have (max Mew-114) - Stores move's code
 	int type[]=new int[2]; // There are 18 types, as of ORAS 
 		//	^^^^
 		//	maximo dois tipos
