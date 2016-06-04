1085f769bec5b6e3860de66eaf0690c9cb77c126
cdflion <44637g@gmail.com>
2016-06-04 11:04:33 -0300

    Update Bnode.java

--- Bnode.java
+++ Bnode.java
@@ -1,24 +1,11 @@
+
 package btreetest;
 
-
-public class Bnode {
-    public int i;
-    public int pai;         //indice no array do pai
-    public Blmnt[] itens;   //array de elementos do nodo
-    
-    public Bnode(int i, int pai){
-        itens=new Blmnt[2*i];       //cada item ja eh inicializado vazio
-        this.pai=pai; 
-        this.i=i;
-    }
-    public boolean folha(){
-        if(itens[0].filhos[0]==-1)
-            return true;
-        else return false;
-    }
-    public boolean cheio(){
-        if(itens.length==2*i)
-            return true;
-        else return false;
-    }
+public class Bnode implements java.io.Serializable{
+    public int t=2;    //n minimo de chaves                     //2 soh para fins de teste
+    public int nchaves;
+    public boolean folha;
+    public int[] chaves= new int[2*t];  //para cada chave um offset no arquivo
+    public int[] pointer=new int[2*t+1];//nodos filhos
+    public int[] offset= new int[2*t];
 }
