49507c0553982041689da8d3e80dc7c05dbdc305
cdflion <44637g@gmail.com>
2016-06-04 18:59:15 -0300

    Update BtreeTest.java

--- BtreeTest.java
+++ BtreeTest.java
@@ -10,13 +10,17 @@ import java.util.logging.Logger;
 
 public class BtreeTest {
     //Regra Geral: se eh -1 entao n existe
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
+        
         generateEx("arq3");
-        //Btree arvi=new Btree("arq3");
+        Btree arvi=new Btree("arq3");
+        //arvi.insere(44, 440);
+        
+        arvi.printTree();
         
     }
-    public static void generateEx(String fName){
-        Bnode[] itens=new Bnode[6];
+    public static void generateEx(String fName){    //gera um arquivo com uma 
+        Bnode[] itens=new Bnode[6];                 //arvore B generica
         for (int i = 0; i < 6; i++) {
             itens[i] = new Bnode();
         }
@@ -35,6 +39,7 @@ public class BtreeTest {
         itens[0].pointer[2]=3;
         itens[0].pointer[3]=4;
         itens[0].pointer[4]=5;
+        itens[0].papai=-1;      //a raiz n tem pai
         
         itens[1].chaves[0]=3;
         itens[1].chaves[1]=5;
@@ -44,6 +49,7 @@ public class BtreeTest {
         itens[1].offset[0]=30;
         itens[1].offset[1]=50;
         itens[1].offset[2]=80;
+        itens[1].papai=0;
 
         itens[2].chaves[0]=13;
         itens[2].chaves[1]=15;
@@ -53,6 +59,7 @@ public class BtreeTest {
         itens[2].offset[0]=130;
         itens[2].offset[1]=150;
         itens[2].offset[2]=180;
+        itens[2].papai=0;
         
         itens[3].chaves[0]=23;
         itens[3].chaves[1]=25;
@@ -62,6 +69,7 @@ public class BtreeTest {
         itens[3].offset[0]=230;
         itens[3].offset[1]=250;
         itens[3].offset[2]=280;
+        itens[3].papai=0;
         
         itens[4].chaves[0]=33;
         itens[4].chaves[1]=35;
@@ -71,6 +79,7 @@ public class BtreeTest {
         itens[4].offset[0]=330;
         itens[4].offset[1]=350;
         itens[4].offset[2]=380;
+        itens[4].papai=0;
         
         itens[5].chaves[0]=43;
         itens[5].chaves[1]=45;
@@ -80,14 +89,16 @@ public class BtreeTest {
         itens[5].offset[0]=430;
         itens[5].offset[1]=450;
         itens[5].offset[2]=480;
+        itens[5].papai=0;
         
         try{
             //1 - Crie um objeto FileOutputStream
             FileOutputStream fileStream = new FileOutputStream(fName);
             //2 - Crie um ObjectOutputStream
             ObjectOutputStream os = new ObjectOutputStream(fileStream);
-            //3 - Grava quantos tem
+            //3 - Grave quantos tem e raiz
             os.writeInt(6);
+            os.writeInt(0);
             //4 - Grave os objetos
             os.writeObject(itens[0]); 
             os.writeObject(itens[1]);
