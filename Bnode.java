5a0589a549a05315b40b397fd559197985bbe318
cdflion <44637g@gmail.com>
2016-06-04 12:56:45 -0300

    Update Bnode.java
    Add metodo para imprimir o nodo para fins de teste.

--- Bnode.java
+++ Bnode.java
@@ -8,4 +8,11 @@ public class Bnode implements java.io.Serializable{
     public int[] chaves= new int[2*t];  //para cada chave um offset no arquivo
     public int[] pointer=new int[2*t+1];//nodos filhos
     public int[] offset= new int[2*t];
+    
+    public void printNode(){
+        System.out.println("nchaves="+nchaves+", folha:"+folha);
+        for(int i=0; i<nchaves; i++){
+            System.out.println(chaves[i]+" - "+offset[i]);
+        }
+    }
 }
