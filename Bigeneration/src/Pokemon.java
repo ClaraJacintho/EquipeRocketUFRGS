699ea6311c6130ba215c2ad95c46fd232219541c
SpaceCenturion <mcmachry@gmail.com>
2016-06-09 19:00:48 -0300

    Serializable
    Implements Serializeble! Para salvar em arquivos binarios.

--- Bigeneration/src/Pokemon.java
+++ Bigeneration/src/Pokemon.java
@@ -1,13 +1,15 @@
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Pokemon {
+public class Pokemon implements Serializable{
+	private static final long serialVersionUID = 4640315127286444098L;
+	
 	String name;
 	int code;	//Pokedex Code 1-720
 	int evolution;	//Code of this Pokemons evolution
 	int prevolution;	//The Pokemon that evolves into this 
 	
-	ArrayList<Integer> moves = new ArrayList<>();   //List of all possible moves this pokemon can have (max Mew-114) - Stores move's code
-	int type[]=new int[2]; // There are 18 types, as of ORAS 
-		//	^^^^
-		//	maximo dois tipos
+	ArrayList<Integer> moves = new ArrayList<>();  //List of all possible moves this pokemon can have (max Mew-114) - Stores move's code
+	int type[] = new int[2]; // There are 18 types, as of ORAS 
 }
+
